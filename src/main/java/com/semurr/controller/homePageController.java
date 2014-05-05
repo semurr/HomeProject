76d99b5cc7fe228737a6d5acd4026862dd48462c
Stephen Murray <smurray@live.com>
2014-05-04 21:50:50 -0700

    added blogs title to front page

--- src/main/java/com/semurr/controller/homePageController.java
+++ src/main/java/com/semurr/controller/homePageController.java
@@ -1,5 +1,7 @@
 package com.semurr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.semurr.dao.BlogDAO;
+import com.semurr.dao.impl.BlogDAOImpl;
+import com.semurr.model.Blog;
 import com.semurr.model.SessionData;
 
 @Controller
@@ -17,8 +22,18 @@ public class homePageController {
 	@Autowired
 	private SessionData sessionData;
 	
+	private BlogDAO blogDAO;
+	
 	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public ModelAndView getHomePage(ModelMap model){
+		
+		//TODO: autowire
+		blogDAO = new BlogDAOImpl();
+		
+		//get blog
+		List<Blog> blogs = blogDAO.getAllBlogs();
+		
+		model.addAttribute("blogList", blogs);
 		
 		
 		return new ModelAndView("index", model);		
--- src/main/webapp/WEB-INF/ftl/index.ftl
+++ src/main/webapp/WEB-INF/ftl/index.ftl
@@ -1,4 +1,7 @@
 <#import "layout/defaultLayout.ftl" as layout>
 <@layout.defaultLayout>
-	<h1>FreemarkerLayout</h1>	
+	<h1 align="center">Blogs:</h1>
+	<#list blogList as blog>
+	<p>${blog.title}</p>
+	</#list>
 </@layout.defaultLayout>
